c9d642c9e23aa20cad263b2e20714e0382646936
chris_kulhanek <156239549+chriskulhan@users.noreply.github.com>
2025-02-06 16:39:31 +0000

    finished_quest

---
+++ 4_practical_exercises.py
@@ -0,0 +1,53 @@
+#Create a Dictionary
+
+    # Create a dictionary of favorite fast food items
+fast_food_dict = {
+        "burger": "Cheeseburger",
+        "fries": "Curly fries",
+        "drink": "Cherry cola"
+    }
+
+#Create a Tuple
+    # Create a tuple of menu specials
+menu_specials = ("Double Bacon Burger", "Sweet Potato Fries", "Root Beer Float")
+
+#Create a List
+    # Create a list of customer orders
+customer_orders = ["Cheeseburger", "Curly fries", "Cherry cola", "Onion rings"]
+
+#Create a Set
+ # Create a set of unique ingredients
+unique_ingredients = {"beef patty", "cheddar cheese", "lettuce", "ketchup", "pickles"}
+
+#Display the Data
+# Display our fast food feast!
+print("Welcome to Fast Food Fun!")
+print("Today's specials:")
+for special in menu_specials:
+    print("- " + special)
+print("\nCustomer orders:")
+for order in customer_orders:
+    print("- " + order)
+print("\nUnique ingredients we're working with:")
+for ingredient in unique_ingredients:
+    print("- " + ingredient)
+
+# #output: 
+# Welcome to Fast Food Fun!
+# Today's specials:
+# - Double Bacon Burger
+# - Sweet Potato Fries
+# - Root Beer Float
+
+# Customer orders:
+# - Cheeseburger
+# - Curly fries
+# - Cherry cola
+# - Onion rings
+
+# Unique ingredients we're working with:
+# - ketchup
+# - pickles
+# - beef patty
+# - cheddar cheese
+# - lettuce    
